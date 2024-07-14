db7383871ffbf69740e61e2cfe7f126e8ed43178
LalsaPandit99 <pandit.lalsa99@gmail.com>
2024-07-14 17:05:23 -0400

    my commit

---
+++ Firstprogram.py
@@ -0,0 +1,53 @@
+# first_num = int(input("Enter first num: "))
+# second_num = int(input("Enter second num: "))
+# sum = first_num + second_num
+# print("sum :", sum)
+
+# name = input("enter first name: ")
+# countname = len(name)
+# print(countname)
+
+# str = "I $ an$ to Count $$ In the $$ "
+# print(str.count("$"))
+
+
+# nums = int(input("enter any number: "))
+# rem = nums%2
+
+# if (rem == 0):
+#     print("Even")
+# else:
+#     print("odd")
+
+#movies print in list
+
+# movies = []
+# m1 = input("enter first movies: ")
+# m2 = input("enter second movies: ")
+# m3 = input("enter third movies: ")
+
+# movies.append(m1)
+# movies.append(m2)
+# movies.append(m3)
+
+# print(movies)
+
+
+#checking for palindrome
+
+# list1 = [1,2,2,1]
+
+# copy_of_list1 = list1.copy()
+# copy_of_list1.reverse()
+
+# if (copy_of_list1 == list1):
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+
+disc = {
+    "cat": "a small animal",
+    "table" : ["meaning one", "meaning 2 ",]
+}
+print(disc)
